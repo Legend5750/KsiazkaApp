fbc87ac3ff60739b27e0a8818a02cfa108f780d3
Nazar <qwertr@gmail.com>
2026-02-02 12:02:57 +0100

    implementacja klasy książka

---
+++ KsiazkaApp/Ksiazka.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsiazkaApp
+{
+    internal class Ksiazka
+    {
+        public List<Strona> strony = new List<Strona>();
+        public Ksiazka(string tytul) 
+        {
+            Strona strona = new Strona(tytul, 1);
+            strony.Add(strona);
+        }
+        public void DodajStrone(string trescStrony)
+        {
+            var strona = new Strona(trescStrony);
+            strony.Add(strona);
+        }
+    }
+}
