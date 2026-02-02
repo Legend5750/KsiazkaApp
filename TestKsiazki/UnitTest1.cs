e5d84afde1b9769db769085c7c9ee5a426d5b367
Nazar <qwertr@gmail.com>
2026-02-02 13:39:23 +0100

    optymalizacja kodu

--- TestKsiazki/UnitTest1.cs
+++ TestKsiazki/UnitTest1.cs
@@ -5,6 +5,19 @@ namespace TestKsiazki
     [TestClass]
     public class UnitTest1
     {
+        public Ksiazka ZwrocKsiazke()
+        {
+            string daneZPliku = File.ReadAllText(@"C:\Users\Student\Desktop\Dane.txt");
+            string[] tablicaStron = daneZPliku.Split(";");
+
+            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
+
+            for (int i = 1; i < tablicaStron.Length; i++)
+            {
+                ksiazka.DodajStrone(tablicaStron[i]);
+            }
+            return ksiazka;
+        }
         [TestMethod]
         public void TestowaniePierwszejStrony()
         {
@@ -18,36 +31,16 @@ namespace TestKsiazki
         [TestMethod]
         public void TestowanieWczytywaniaZPliku()
         {
-            string daneZPliku = File.ReadAllText(@"C:\Users\Student\Desktop\Dane.txt");
-            string[] tablicaStron = daneZPliku.Split(";");
-
-            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
-
-            for (int i = 1; i < tablicaStron.Length; i++)
-            {
-                ksiazka.DodajStrone(tablicaStron[i]);
-            }
-
+            Ksiazka ksiazka = ZwrocKsiazke();
             string daneDoTestowania = "Powrót do miasta nie by³ ju¿ ucieczk¹, lecz wyborem. Ulice wydawa³y siê jaœniejsze, a ludzie mniej obcy. Adam zatrzyma³ siê przy tym samym zegarze, który kiedyœ go niepokoi³. Tym razem uœmiechn¹³ siê, widz¹c jego niedok³adnoœæ. Czas nie by³ wrogiem, lecz towarzyszem. Wiedzia³, ¿e przed nim jeszcze wiele niewiadomych, ale nie ba³ siê ich jak dawniej. Z listem schowanym g³êboko w kieszeni ruszy³ przed siebie, gotów pisaæ dalsz¹ czêœæ w³asnej historii.";
 
-
-
-            Assert.AreEqual(tablicaStron[tablicaStron.Length - 1].Trim(), daneDoTestowania);
+            Assert.AreEqual(ksiazka.strony[ksiazka.strony.Count - 1].trescStrony.Trim(), daneDoTestowania);
         }
         [TestMethod]
         public void TestowanieIlosciStron()
         {
-            string daneZPliku = File.ReadAllText(@"C:\Users\Student\Desktop\Dane.txt");
-            string[] tablicaStron = daneZPliku.Split(";");
-
-            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
-
-            for (int i = 1; i < tablicaStron.Length; i++)
-            {
-                ksiazka.DodajStrone(tablicaStron[i]);
-            }
-
-            Assert.AreEqual(tablicaStron.Length, 10);
+            Ksiazka ksiazka = ZwrocKsiazke();
+            Assert.AreEqual(ksiazka.strony.Count, 10);
         }
     }
 }
